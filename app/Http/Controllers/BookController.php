ded3a80683ec9d739e8200f771e111f958aac1c0
Pooyan Ghanbari <93377835+PooyanGnb@users.noreply.github.com>
2024-04-24 00:27:03 +0330

    caching
    just made some changes in caching

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Book;
+use Illuminate\Support\Facades\Cache;
 
 class BookController extends Controller
 {
@@ -27,7 +28,10 @@ class BookController extends Controller
             default => $books->latest()
         };
 
-        $books = $books->get();
+        $cacheKey = 'books' . $filter . ':' . $title;
+        $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
+
+        
 
         // return view('books.index', compact('books'));
         return view('books.index', ['books' => $books]);
@@ -54,15 +58,12 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
-        
-        return view(
-            'books.show',
-            [
-                'book' => $book->load([
-                    'reviews' => fn ($query) => $query->latest()
-                ])
-            ]
-        );
+        $cacheKey = 'books' . $book->id;
+        $book = cache()->remember($cacheKey, 3600 , fn() => $book->load([
+            'reviews' => fn ($query) => $query->latest()
+        ]));
+
+        return view('books.show',['book' => $book]);
     }
 
     /**
